378a6c3c7cab41b5e532f3863a99ca4ea64e7306
Станкевич Максим <maksimstank@gmail.com>
2025-12-04 22:09:27 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-import { similarPhotoDescriptions } from './data';
-similarPhotoDescriptions();
+// main.js
+import { similarPhotoDescriptions } from './data.js';
+import { initThumbnails } from './thumbnail-renderer.js';
+
+// Инициализируем отрисовку миниатюр при загрузке страницы
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Данные фотографий загружены:', similarPhotoDescriptions.length, 'шт.');
+  initThumbnails();
+});
+
+// Экспортируем данные для использования в других модулях
+export { similarPhotoDescriptions };
