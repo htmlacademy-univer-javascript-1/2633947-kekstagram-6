7d87a5dba1fd9daa8d681d2cf53a38faf6ac85d7
Станкевич Максим <maksimstank@gmail.com>
2025-12-29 19:27:50 +0700

    commit

--- js/image-filter.js
+++ js/image-filter.js
@@ -2,35 +2,39 @@ import {imageRender, clearPictures} from './gallery-manager.js';
 import {delayCall, randomizeArray} from './util.js';
 import {getPhotos} from './main.js';
 
-const FILTER_LIMIT = 10;
+// Максимальное количество фотографий для случайного фильтра
+const RANDOM_FILTER_LIMIT = 10;
+// CSS класс активной кнопки фильтра
 const ACTIVE_BUTTON_CLASS = 'img-filters__button--active';
+// Секция фильтров изображений
+const filterSection = document.querySelector('.img-filters');
+// Форма с кнопками фильтров
+const filterForm = filterSection.querySelector('.img-filters__form');
+// Проверяет, является ли элемент кнопкой
+const isButtonElement = (evt) => evt.target.tagName === 'BUTTON';
 
-const imgFilters = document.querySelector('.img-filters');
-const imgFiltersForm = imgFilters.querySelector('.img-filters__form');
-
-const isButton = (evt) => evt.target.tagName === 'BUTTON';
-
-const availableFilters = {
+// Объект с доступными фильтрами
+const filterStrategies = {
   'filter-default': () => getPhotos(),
   'filter-random': () => {
     const allPhotos = getPhotos();
     const shuffled = randomizeArray(allPhotos);
-    return shuffled.slice(0, FILTER_LIMIT);
+    return shuffled.slice(0, RANDOM_FILTER_LIMIT);
   },
   'filter-discussed': () => getPhotos().sort((firstElement, secondElement) => secondElement.comments.length - firstElement.comments.length
   )
 };
-
+ // Обработчик клика по форме фильтров с задержкой
 const onImgFilterFormClick = delayCall((evt) => {
-  if (isButton(evt) && availableFilters[evt.target.id]) {
+  if (isButtonElement(evt) && filterStrategies[evt.target.id]) {
     clearPictures();
-    imageRender(availableFilters[evt.target.id]());
+    imageRender(filterStrategies[evt.target.id]());
   }
 });
-
+// Обработчик клика для обновления активной кнопки
 const onButtonClick = (evt) => {
-  if (isButton(evt) && availableFilters[evt.target.id]) {
-    const selectedButton = imgFiltersForm.querySelector(`.${ACTIVE_BUTTON_CLASS}`);
+  if (isButtonElement(evt) && filterStrategies[evt.target.id]) {
+    const selectedButton = filterForm.querySelector(`.${ACTIVE_BUTTON_CLASS}`);
     if (selectedButton) {
       selectedButton.classList.remove(ACTIVE_BUTTON_CLASS);
     }
@@ -38,5 +42,5 @@ const onButtonClick = (evt) => {
   }
 };
 
-imgFiltersForm.addEventListener('click', onImgFilterFormClick);
-imgFiltersForm.addEventListener('click', onButtonClick);
+filterForm.addEventListener('click', onImgFilterFormClick); // Назначение обработчика фильтрации
+filterForm.addEventListener('click', onButtonClick); // Назначение обработчика обновления кнопок
