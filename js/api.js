1c01a635c461b6184d33524b21429884dca65c77
Станкевич Максим <maksimstank@gmail.com>
2025-12-29 22:40:53 +0700

    Merge branch 'master' into module12-task2git status

--- js/api.js
+++ js/api.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const API_ENDPOINTS = {
   GET: 'https://29.javascript.htmlacademy.pro/kekstagram/data', // URL для получения данных
   POST: 'https://29.javascript.htmlacademy.pro/kekstagram/', // URL для отправки данных
@@ -30,44 +29,3 @@ const submitPhotoData = (successCallback, errorCallback, httpMethod = 'POST', bo
 };
 
 export { fetchPhotoData, submitPhotoData };
-=======
-const BASE_URL = 'https://29.javascript.htmlacademy.pro/kekstagram';
-
-const Route = {
-  GET_DATA: '/data',
-  SEND_DATA: '/'
-};
-
-const Method = {
-  GET: 'GET',
-  POST: 'POST'
-};
-
-const ErrorText = {
-  [Method.GET]: 'Не удалось загрузить данные. Попробуйте обновить страницу',
-  [Method.POST]: 'Ошибка загрузки файла'
-};
-
-async function request(url, method = Method.GET, body = null) {
-  const response = await fetch(`${BASE_URL}${url}`, {
-    method,
-    body
-  });
-
-  if (!response.ok) {
-    throw new Error(ErrorText[method]);
-  }
-
-  return response.json();
-}
-
-async function loadPhotos() {
-  return request(Route.GET_DATA);
-}
-
-async function sendForm(formData) {
-  return request(Route.SEND_DATA, Method.POST, formData);
-}
-
-export { loadPhotos, sendForm };
->>>>>>> 5b08c58b69231c934818936e0997605e6f8312c9
