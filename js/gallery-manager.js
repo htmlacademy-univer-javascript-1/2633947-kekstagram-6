c33dba92db54ff8997eeed15d89eeb25ca0faaa6
Станкевич Максим <maksimstank@gmail.com>
2025-12-29 19:11:05 +0700

    commit

--- js/gallery-manager.js
+++ js/gallery-manager.js
@@ -1,10 +1,13 @@
 import { expandImage } from './image-detail-view.js';
-const pictures = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
+// Контейнер для галереи изображений
+const pictures = document.querySelector('.pictures');
+// Шаблон миниатюры изображения
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+// Создает DOM-элемент миниатюры изображения
 const renderPicture = (picture)=>{
-  const {url, description, comments, likes} = picture;
-  const pictureElement = pictureTemplate.cloneNode(true);
+  const {url, description, comments, likes} = picture; // Деструктуризация данных изображения
+  const pictureElement = pictureTemplate.cloneNode(true); // Клонирование шаблона
 
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__img').alt = description;
@@ -19,6 +22,7 @@ const renderPicture = (picture)=>{
   return pictureElement;
 };
 
+// Отрисовывает коллекцию изображений в галерее
 const imageRender = (objects)=>{
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < objects.length; i++){
@@ -27,8 +31,10 @@ const imageRender = (objects)=>{
   pictures.appendChild(fragment);
 };
 
+// Получение всех миниатюр в галерее
 const photos = pictures.getElementsByClassName('picture');
 
+// Очищает все изображения из галереи
 const clearPictures = ()=>{
   if (photos){
     [...photos].forEach((photo) => photo.remove());
